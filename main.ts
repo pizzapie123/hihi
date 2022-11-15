c9e5156d36cbf1961accd6a8b32d97b96d5a6db4
pizzapie123 <117457376+pizzapie123@users.noreply.github.com>
2022-11-15 09:40:18 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+basic.showString("Hello!")
+basic.pause(100)
+basic.showString("123")
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # . . # #
+    . . # . #
+    # . # . #
+    # # . . #
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    # # # # #
+    . # . # .
+    `)
+basic.pause(100)
+basic.forever(function () {
+	
+})
